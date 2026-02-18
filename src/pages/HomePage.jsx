0727100ce7cb6a3a16b321eadc0922febd10cc39
DylanC <comeaudc@comcast.net>
2026-02-18 13:40:17 -0500

    Added Data Fetch to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,23 @@ import axios from "axios";
 export default function HomePage() {
   const [characters, setCharacters] = useState(null);
 
-  function handleFetch(e){
-    console.log('testing')
+  async function handleFetch(e) {
+    try {
+      let res = await axios.get("http://localhost:3000/api/char");
+
+      setCharacters(res.data);
+    } catch (err) {
+      console.error(err.message);
+    }
   }
 
   return (
     <div className="home">
-      {characters ? <h1>Data we Fetched</h1> : <button onClick={handleFetch}>Fetch Data</button>}
+      {characters ? (
+        <h1>Data we Fetched</h1>
+      ) : (
+        <button onClick={handleFetch}>Fetch Data</button>
+      )}
     </div>
   );
 }
