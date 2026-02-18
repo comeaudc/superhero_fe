ae51e125e7456dbb60a84e748fe2e18ba47dd5f1
DylanC <comeaudc@comcast.net>
2026-02-18 14:09:49 -0500

    Added Delete capabilities

--- src/components/CharCard/CharCard.jsx
+++ src/components/CharCard/CharCard.jsx
@@ -1,5 +1,17 @@
+import axios from "axios";
+
 export default function CharCard({ char }) {
   let powerList = char.powers.map((power) => <li>{power}</li>);
+
+  async function handleDelete() {
+    try {
+      await axios.delete(`http://localhost:3000/api/char/${char._id}`);
+      alert("Successful deletion");
+    } catch (error) {
+      console.error(error.message);
+      alert(error.message);
+    }
+  }
 
   return (
     <div className="card">
@@ -8,6 +20,8 @@ export default function CharCard({ char }) {
       {char.hero ? <p>SuperHero</p> : <p>SuperVillan</p>}
       <h3>Powers:</h3>
       <ul>{powerList}</ul>
+      <button>Edit</button>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
