214f993279ffa343b924b63e1a8416e95e7db327
DylanC <comeaudc@comcast.net>
2026-02-18 14:00:57 -0500

    Updated CharCard

--- src/components/CharCard/CharCard.jsx
+++ src/components/CharCard/CharCard.jsx
@@ -1,9 +1,13 @@
 export default function CharCard({ char }) {
+  let powerList = char.powers.map((power) => <li>{power}</li>);
+
   return (
     <div className="card">
       <h2>{char.name}</h2>
       <h3>{char.alias}</h3>
       {char.hero ? <p>SuperHero</p> : <p>SuperVillan</p>}
+      <h3>Powers:</h3>
+      <ul>{powerList}</ul>
     </div>
   );
 }
